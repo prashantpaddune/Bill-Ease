95c848a679b05103968366caa753a7498fc976c0
prashantpaddune <padadunep@gmail.com>
2023-11-16 23:19:05 +0530

    chore: basic layout
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- src/components/CreateInvoice/index.js
+++ src/components/CreateInvoice/index.js
@@ -1,10 +1,70 @@
+import React from 'react';
+import {useFieldArray, useForm} from "react-hook-form";
+import { InvoiceContainer, Button, ErrorMessage, LineItem, LineItemsContainer, LineInput, Input, Header, Label, Title, TextArea, FieldSet} from "./styles";
 
-const CreateInvoice = () => {
+const Invoice = () => {
+    const { register, control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            lineItems: [{ description: '', quantity: 1, rate: 0 }],
+        },
+    });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: 'lineItems',
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
-        <div>
-        <h1>Create Invoice</h1>
-        </div>
-    );
-}
+        <InvoiceContainer>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Header>
+                    <Title>Invoice</Title>
+                </Header>
 
-export default CreateInvoice;
+                <FieldSet>
+                    <Label>Invoice Number</Label>
+                    <Input {...register('invoiceNumber', { required: 'Invoice number is required' })} type="text" placeholder="1234" />
+                    {errors.invoiceNumber && <ErrorMessage>{errors.invoiceNumber.message}</ErrorMessage>}
+                </FieldSet>
+
+                <FieldSet>
+                    <Label>Bill To</Label>
+                    <TextArea {...register('billTo', { required: 'Billing information is required',  })} placeholder="Client's Name and Address" />
+                    {errors.billTo && <ErrorMessage>{errors.billTo.message}</ErrorMessage>}
+                </FieldSet>
+
+                <FieldSet>
+                    <Label>Ship To</Label>
+                    <TextArea {...register('shipTo', { required: 'Shipping information is required' })} placeholder="Shipping Address"/>
+                    {errors.shipTo && <ErrorMessage>{errors.shipTo.message}</ErrorMessage>}
+                </FieldSet>
+
+                    <Button type="button" onClick={() => append({ description: '', quantity: 1, rate: 0 })}>
+                        Add Line Item
+                    </Button>
+
+                <LineItemsContainer>
+                    {fields.map((item, index) => (
+                        <LineItem key={item.id}>
+                            <LineInput {...register(`lineItems.${index}.description`, { required: 'Description is required' })} type="text" placeholder="Description" />
+                            <LineInput {...register(`lineItems.${index}.quantity`, { required: 'Quantity is required' })} type="number" placeholder="Quantity" />
+                            <LineInput {...register(`lineItems.${index}.rate`, { required: 'Rate is required' })} type="number" placeholder="Rate" />
+                        </LineItem>
+                    ))}
+                </LineItemsContainer>
+
+                <FieldSet>
+                    <Label>Remarks</Label>
+                    <TextArea {...register('Remark')} rows="4" />
+                </FieldSet>
+                <Button type="submit">Submit Invoice</Button>
+            </form>
+        </InvoiceContainer>
+    );
+};
+
+export default Invoice;
---
+++ src/components/CreateInvoice/styles.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+const colors = {
+    primary: '#4a4e69',
+    secondary: '#9a8c98',
+    background: '#f2e9e4',
+    light: '#c9ada7',
+    dark: '#22223b',
+};
+
+const InvoiceContainer = styled.div`
+  background: ${colors.background};
+  color: ${colors.dark};
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+`;
+
+const Header = styled.div`
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: ${colors.primary};
+`;
+
+const FieldSet = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+  color: ${colors.primary};
+  font-weight: 600;
+`;
+
+const Input = styled.input`
+  width: 98%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid ${colors.light};
+  border-radius: 4px;
+  font-size: 0.9rem;
+`;
+
+const TextArea = styled.textarea`
+  width: 98%;
+  padding: 10px;
+  border: 1px solid ${colors.light};
+  border-radius: 4px;
+  resize: vertical;
+  font-size: 0.9rem;
+  height: 80px;
+`;
+
+const LineItemsContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const LineItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const LineInput = styled(Input)`
+  flex: 1;
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: ${colors.primary};
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 0.9rem;
+  &:hover {
+    background-color: ${colors.dark};
+  }
+`;
+
+const ErrorMessage = styled.span`
+  color: red;
+  font-size: 0.8rem;
+`;
+
+export {
+    InvoiceContainer,
+    Header,
+    Title,
+    FieldSet,
+    Label,
+    Input,
+    TextArea,
+    LineItemsContainer,
+    LineItem,
+    LineInput,
+    Button,
+    ErrorMessage
+}
